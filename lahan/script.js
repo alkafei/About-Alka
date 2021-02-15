c455ea315108d5ce38ffc9556e21ff464031a912
Alka <39877276+alkafei@users.noreply.github.com>
2021-02-15 21:37:42 +0700

    Add files via upload

---
+++ lahan/script.js
@@ -0,0 +1,164 @@
+// Mythium Archive: https://archive.org/details/mythium/
+
+jQuery(function ($) {
+    'use strict'
+    var supportsAudio = !!document.createElement('audio').canPlayType;
+    if (supportsAudio) {
+        // initialize plyr
+        var player = new Plyr('#audio1', {
+            controls: [
+                'restart',
+                'play',
+                'progress',
+                'current-time',
+                'duration',
+                'mute',
+                'volume',
+                'download'
+            ]
+        });
+        // initialize playlist and controls
+        var index = 0,
+            playing = false,
+            mediaPath = 'https://archive.org/download/lahan/',
+            extension = '',
+            tracks = [{
+                "track": 1,
+                "name": "FLILV - Something to Gain After the Pain",
+                "duration": "3:32",
+                "file": "Fear, and Loathing in Las Vegas - Something to Gain After the Pain"
+            }, {
+                "track": 2,
+                "name": "TWICE - Feel Special",
+                "duration": "3:27",
+                "file": "TWICE - Feel Special"
+            }, {
+                "track": 3,
+                "name": "Little Big - Big Dick",
+                "duration": "2:40",
+                "file": "Little Big - Big Dick"
+            }, {
+                "track": 4,
+                "name": "FLILV - Abyss",
+                "duration": "3:46",
+                "file": "Fear, and Loathing in Las Vegas - Abyss"
+            }, {
+                "track": 5,
+                "name": "TWICE - FANCY",
+                "duration": "3:34",
+                "file": "TWICE - FANCY"
+            }, {
+                "track": 6,
+                "name": "FLILV - LLLD",
+                "duration": "3:17",
+                "file": "Fear, and Loathing in Las Vegas - LLLD"
+            }, {
+                "track": 7,
+                "name": "Goose house - Swing Girl",
+                "duration": "3:40",
+                "file": "Goose house - Swing Girl"
+            }, {
+                "track": 8,
+                "name": "FLILV - Crossover.mp3",
+                "duration": "3:12",
+                "file": "Fear, and Loathing in Las Vegas - Crossover"
+            }, {
+                "track": 9,
+                "name": "TWICE - YES or YES",
+                "duration": "3:58",
+                "file": "TWICE - YES or YES"
+            }],
+            buildPlaylist = $.each(tracks, function(key, value) {
+                var trackNumber = value.track,
+                    trackName = value.name,
+                    trackDuration = value.duration;
+                if (trackNumber.toString().length === 1) {
+                    trackNumber = '0' + trackNumber;
+                }
+                $('#plList').append('<li> \
+                    <div class="plItem"> \
+                        <span class="plNum">' + trackNumber + '.</span> \
+                        <span class="plTitle">' + trackName + '</span> \
+                        <span class="plLength">' + trackDuration + '</span> \
+                    </div> \
+                </li>');
+            }),
+            trackCount = tracks.length,
+            npAction = $('#npAction'),
+            npTitle = $('#npTitle'),
+            audio = $('#audio1').on('play', function () {
+                playing = true;
+                npAction.text('Now Playing...');
+            }).on('pause', function () {
+                playing = false;
+                npAction.text('Paused...');
+            }).on('ended', function () {
+                npAction.text('Paused...');
+                if ((index + 1) < trackCount) {
+                    index++;
+                    loadTrack(index);
+                    audio.play();
+                } else {
+                    audio.pause();
+                    index = 0;
+                    loadTrack(index);
+                }
+            }).get(0),
+            btnPrev = $('#btnPrev').on('click', function () {
+                if ((index - 1) > -1) {
+                    index--;
+                    loadTrack(index);
+                    if (playing) {
+                        audio.play();
+                    }
+                } else {
+                    audio.pause();
+                    index = 0;
+                    loadTrack(index);
+                }
+            }),
+            btnNext = $('#btnNext').on('click', function () {
+                if ((index + 1) < trackCount) {
+                    index++;
+                    loadTrack(index);
+                    if (playing) {
+                        audio.play();
+                    }
+                } else {
+                    audio.pause();
+                    index = 0;
+                    loadTrack(index);
+                }
+            }),
+            li = $('#plList li').on('click', function () {
+                var id = parseInt($(this).index());
+                if (id !== index) {
+                    playTrack(id);
+                }
+            }),
+            loadTrack = function (id) {
+                $('.plSel').removeClass('plSel');
+                $('#plList li:eq(' + id + ')').addClass('plSel');
+                npTitle.text(tracks[id].name);
+                index = id;
+                audio.src = mediaPath + tracks[id].file + extension;
+                updateDownload(id, audio.src);
+            },
+            updateDownload = function (id, source) {
+                player.on('loadedmetadata', function () {
+                    $('a[data-plyr="download"]').attr('href', source);
+                });
+            },
+            playTrack = function (id) {
+                loadTrack(id);
+                audio.play();
+            };
+        extension = audio.canPlayType('audio/mpeg') ? '.mp3' : audio.canPlayType('audio/ogg') ? '.ogg' : '';
+        loadTrack(index);
+    } else {
+        // no audio support
+        $('.column').addClass('hidden');
+        var noSupport = $('#audio1').text();
+        $('.container').append('<p class="no-support">' + noSupport + '</p>');
+    }
+});
